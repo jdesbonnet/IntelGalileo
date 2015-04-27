7a176b7bd40ca10048178c3dae6521806869f6ba
Joe Desbonnet <jdesbonnet@gmail.com>
2015-04-27 13:56:46 +0100

    add some info in comments

--- install-missing-packages.sh
+++ install-missing-packages.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+#
+# Install dependencies needed to build Galileo board software. 
+# Assuming the starting point is a  a fresh Ubuntu Server 12.04 i32 dist. 
+#
+# Joe Desbonnet, jdesbonnet@gmail.com, 27 April 2015.
+# Public domain.
+#
 rm /var/lib/apt/lists/*
 apt-get update
 apt-get upgrade
