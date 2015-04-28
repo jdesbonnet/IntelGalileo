10f1b1d4e63b861b3c8ca19ddf2dcc1d6db84e92
galileo <galileo@GalileoBuildServer.(none)>
2015-04-28 22:38:58 +0100

    updates to build script

--- build.sh
+++ build.sh
@@ -14,3 +14,15 @@ tar -xf meta-clanton_v1.0.5.tar.gz
 cd meta-clanton_v1.0.5
 source poky/oe-init-build-env yocto_build
 bitbake image-spi-galileo
+
+tar -xf Quark_EDKII_v1.0.2.tar.gz
+./patches_v1.0.5/patch.Quark_EDKII.sh 
+./svn_setup.py
+svn update
+
+export GCCVERSION=$(gcc -dumpversion | cut -c 3)
+
+# This is currently failing due to the following:
+# GenFvInternalLib.c:26:23: fatal error: uuid/uuid.h: No such file or directory
+./quarkbuild.sh -r32 GCC4${GCCVERSION} QuarkPlatform
+
